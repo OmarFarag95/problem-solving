682cf16f31cb778d9d59cdfeaab2c362d7a26722
omar-farag <omar16farag16@gmail.com>
2020-04-17 22:28:50 +0200

    add easy challenges solutions

---
+++ easy/apple_and_orange.java
@@ -0,0 +1,22 @@
+package easy;
+
+public class apple_and_orange {
+	
+	static void countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges) {
+
+		int apples_count = 0;
+		int oranges_count = 0;
+		for (int i = 0; i < apples.length; i++) {
+			if (apples[i] + a >= s && apples[i] + a <= t)
+				apples_count++;
+		}
+		for (int i = 0; i < oranges.length; i++) {
+			if (oranges[i] + b >= s && oranges[i] + b <= t)
+				oranges_count++;
+		}
+		System.out.println(apples_count);
+		System.out.println(oranges_count);
+
+	}
+
+}
