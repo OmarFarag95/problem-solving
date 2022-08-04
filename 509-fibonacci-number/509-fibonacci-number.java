87b7dc1927ca82d0828cdef43c0e6291dac58947
OmarFarag95 <53435988+OmarFarag95@users.noreply.github.com>
2022-08-05 00:27:57 +0200

    Time: 0 ms (100.00%), Space: 41.1 MB (32.32%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -0,0 +1,63 @@
+// recursive solution
+// class Solution {
+//     public int fib(int n) {
+        
+//         if(n==0)
+//             return 0;
+//         if(n==1)
+//             return 1;
+        
+//         return fib(n-1) + fib(n-2);
+        
+//     }
+// }
+
+// optimized solution
+
+// class Solution {
+// //     public int fib(int n) {
+      
+// //         int [] fibs = new int[n+1];
+        
+// //         fibs[0]=0;
+// //         if (n>0)
+// //         {
+// //         fibs[1]=1;
+// //         for(int i=2;i<=n;i++)
+// //             fibs[i] = fibs[i-1] + fibs[i-2];
+// //         }
+        
+// //         return fibs[n];
+// //     }
+// }
+
+
+class Solution {
+    
+    public int fib(int n)
+    {
+        
+        if (n==0)
+            return 0;
+        
+        
+        int a = 0;
+        int b = 1;
+        
+        int sum = 1;
+        
+        int i=2;
+        
+        while(i<=n)
+        {
+            sum = a+b;
+            a = b;
+            b = sum;
+            
+            
+            i++;
+        }
+        
+        return sum;
+    }
+}
