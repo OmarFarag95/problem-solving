fb57d4af22470d62d9581434b829d391cd989ebf
OmarFarag95 <53435988+OmarFarag95@users.noreply.github.com>
2022-08-13 02:09:39 +0200

    Time: 25 ms, Memory: 69.1 MB - LeetHub

--- contains-duplicate/contains-duplicate.java
+++ contains-duplicate/contains-duplicate.java
@@ -1,28 +1,28 @@
-class Solution {
-    public boolean containsDuplicate(int[] nums) {
-        
-        HashSet<Integer> unique = new HashSet<>();
-        
-        for(int i:nums)
-        {
-            if(unique.contains(i))
-                return true;
-            unique.add(i);
-        }
-    
-    return false;
-    }
-}
-
 // class Solution {
 //     public boolean containsDuplicate(int[] nums) {
         
-//         Arrays.sort(nums);
+//         HashSet<Integer> unique = new HashSet<>();
         
-//         for(int i=1;i<nums.length;i++)
-//             if(nums[i]==nums[i-1])
+//         for(int i:nums)
+//         {
+//             if(unique.contains(i))
 //                 return true;
+//             unique.add(i);
+//         }
     
 //     return false;
 //     }
 // }
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        
+        Arrays.sort(nums);
+        
+        for(int i=1;i<nums.length;i++)
+            if(nums[i]==nums[i-1])
+                return true;
+    
+    return false;
+    }
+}
