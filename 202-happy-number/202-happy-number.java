eaefb7bbb10943b0059b664e955d27ace3bcd840
OmarFarag95 <53435988+OmarFarag95@users.noreply.github.com>
2022-08-13 02:01:13 +0200

    Time: 1 ms (98.92%), Space: 39.5 MB (93.52%) - LeetHub

---
+++ 202-happy-number/202-happy-number.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isHappy(int n) {
+        
+        int v = 0;
+        
+        HashSet<Integer> cache = new HashSet<>();
+        
+        while(v!=1)
+        {   
+            v = 0;
+            
+            while(n!=0)
+            {
+                int curr= n%10;
+                n/=10;
+                v+=curr*curr;
+            }
+            
+            
+            if (cache.contains(v))
+                return false;
+            
+            cache.add(v);
+            n = v;
+        }
+        
+        
+        return true;
+    }
+}
