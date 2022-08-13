07f1e130886a421e3f83cee8fc61d9f897a27cd7
OmarFarag95 <53435988+OmarFarag95@users.noreply.github.com>
2022-08-13 04:21:15 +0200

    Time: 3 ms (69.64%), Space: 76.8 MB (76.33%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
@@ -1,21 +1,45 @@
+// class Solution {
+//     public int maxProfit(int[] prices) {
+        
+//         PriorityQueue<Integer> prq = new PriorityQueue<>();
+        
+//         int max_value = Integer.MIN_VALUE;
+        
+//         for(int i=0;i<prices.length;i++)
+//         {
+//             prq.add(prices[i]);
+//             int value = prices[i] - prq.peek();
+            
+//             if(value>max_value)
+//                 max_value = value;
+            
+//         }
+        
+//         return max_value;
+        
+//     }
+// }
+
 class Solution {
-    public int maxProfit(int[] prices) {
+     public int maxProfit(int[] prices) {
         
-        PriorityQueue<Integer> prq = new PriorityQueue<>();
         
-        int max_value = Integer.MIN_VALUE;
+         int max_value = Integer.MIN_VALUE;
+         int min_value = Integer.MAX_VALUE;
         
-        for(int i=0;i<prices.length;i++)
-        {
-            prq.add(prices[i]);
-            int value = prices[i] - prq.peek();
+         for(int i=0;i<prices.length;i++)
+         {
+             if(prices[i]<min_value)
+                 min_value = prices[i];
+             
+             int profit = prices[i] - min_value;
             
-            if(value>max_value)
-                max_value = value;
+             if(profit>max_value)
+                 max_value = profit;
             
-        }
+         }
         
-        return max_value;
+         return max_value;
         
-    }
-}
+     }
+ }
