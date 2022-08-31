b8f87af4b9c4c79009555eb06d883a45cfde9709
OmarFarag95 <53435988+OmarFarag95@users.noreply.github.com>
2022-09-01 00:28:00 +0200

    Time: 102 ms (17.00%), Space: 43 MB (86.39%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -1,39 +1,55 @@
 class Solution {
+//     public int lengthOfLongestSubstring(String s) {
+        
+//         int max_length = 0;
+        
+//         for(int i=0;i<s.length();i++)
+//         {
+//             HashSet<Character> candidate = new HashSet<>();
+            
+//             for(int j=i;j<s.length();j++)
+//             {
+//                 if(candidate.contains(s.charAt(j)))
+//                     break;
+                
+//                 candidate.add(s.charAt(j));
+//             }
+            
+//             if(candidate.size()>max_length)
+//                 max_length = candidate.size();
+            
+//         }
+        
+//         return max_length;
+//     }
+    
     public int lengthOfLongestSubstring(String s) {
         
         int max_length = 0;
+        int i = 0;
+        int j = 0;
         
-        for(int i=0;i<s.length();i++)
+        HashSet<Character> candidate = new HashSet<>();
+        
+        while(j<s.length())
         {
-            HashSet<Character> candidate = new HashSet<>();
-            
-            for(int j=i;j<s.length();j++)
+            if(candidate.contains(s.charAt(j)))
             {
-                if(candidate.contains(s.charAt(j)))
-                    break;
-                
-                candidate.add(s.charAt(j));
+                i++;
+                j=i;
+                candidate.clear();
             }
-            
-            if(candidate.size()>max_length)
-                max_length = candidate.size();
-            
+            else
+            {
+                candidate.add(s.charAt(j));
+                max_length = Math.max(max_length, candidate.size());
+                j++;
+                
+            }
         }
         
         return max_length;
     }
-    
-//     public int lengthOfLongestSubstring(String s) {
-        
-//         int max_length = 0;
-//         int i = 0;
-//         int j = 1;
-        
-//         while(j<s.length())
-//         {
-//             if(s.charAt(j) == )
-//         }
-//     }
     
     
 }
