e9a03bc9f3a4486f8214720c5c75bece6828e6cb
OmarFarag95 <53435988+OmarFarag95@users.noreply.github.com>
2022-08-31 18:27:54 +0200

    Time: 1 ms (100.00%), Space: 49.9 MB (92.90%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.java
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.java
@@ -1,45 +1,70 @@
 class Solution {
-    public int minSubArrayLen(int target, int[] nums) {
+//     public int minSubArrayLen(int target, int[] nums) {
         
         
         
-        int min_length = Integer.MAX_VALUE;
+//         int min_length = Integer.MAX_VALUE;
         
-        if(nums.length>0 && nums[0] >= target)
-            min_length = 1;
+//         if(nums.length>0 && nums[0] >= target)
+//             min_length = 1;
         
         
-        int [] prefix_sum = new int [nums.length];
+//         int [] prefix_sum = new int [nums.length];
         
-        prefix_sum[0] = nums[0];
+//         prefix_sum[0] = nums[0];
         
-        for(int i=1;i<prefix_sum.length;i++)
-            prefix_sum[i] = prefix_sum[i-1] + nums[i];
+//         for(int i=1;i<prefix_sum.length;i++)
+//             prefix_sum[i] = prefix_sum[i-1] + nums[i];
         
     
-        int i = 0;
-        int j = 1;
+//         int i = 0;
+//         int j = 1;
         
         
-        while(i<prefix_sum.length)
+//         while(i<prefix_sum.length)
+//         {
+            
+//             if(j<=prefix_sum.length-1 && prefix_sum[j]>=target)
+//                 min_length = Math.min(min_length, j+1);
+            
+//             if(j<=prefix_sum.length-1 && prefix_sum[j] - prefix_sum[i] >= target)
+//             {
+//                 min_length = Math.min(min_length, j-i);
+//                 i++;
+//             }
+
+//             else if(j<=prefix_sum.length-1 && prefix_sum[j]-prefix_sum[i] < target)
+//                 j++;
+
+//             else i++;
+            
+//         }
+        
+        
+//         if(min_length == Integer.MAX_VALUE)
+//             return 0;
+        
+//         return min_length;
+//     }
+    
+    public int minSubArrayLen(int target, int[] nums) {
+     
+        int l = 0;
+        int r = 0;
+        int sum = 0;
+        int length = 0;
+        int min_length = Integer.MAX_VALUE;
+        for(int i=0;i<nums.length;i++)
         {
+            sum+=nums[i];
             
-            if(j<=prefix_sum.length-1 && prefix_sum[j]>=target)
-                min_length = Math.min(min_length, j+1);
-            
-            if(j<=prefix_sum.length-1 && prefix_sum[j] - prefix_sum[i] >= target)
+            while(sum>=target)
             {
-                min_length = Math.min(min_length, j-i);
-                i++;
+                sum-=nums[l];
+                min_length = Math.min(min_length, i+1 - l);
+                l++;
             }
-
-            else if(j<=prefix_sum.length-1 && prefix_sum[j]-prefix_sum[i] < target)
-                j++;
-
-            else i++;
-            
         }
-        
         
         if(min_length == Integer.MAX_VALUE)
             return 0;
