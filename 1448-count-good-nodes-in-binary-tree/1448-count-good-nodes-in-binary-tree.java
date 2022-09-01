98db42066e425b94ab1b997ba09f35a1effec588
OmarFarag95 <53435988+OmarFarag95@users.noreply.github.com>
2022-09-01 22:43:03 +0200

    Time: 86 ms (5.13%), Space: 67.4 MB (5.02%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
@@ -15,47 +15,43 @@
  */
 class Solution {
     public int goodNodes(TreeNode root) {
-        
-        Stack<TreeNode> nodes = new Stack<>();
-        
-        HashMap<TreeNode,Integer> map = new HashMap<>();
-        
+
+        Stack < TreeNode > nodes = new Stack < > ();
+
+        HashMap < TreeNode, Integer > map = new HashMap < > ();
+
         nodes.push(root);
         map.put(root, Integer.MIN_VALUE);
-        
+
         int count = 0;
-        
-        
-        while(nodes.size()!=0)
-        {
+
+
+        while (nodes.size() != 0) {
             int n = nodes.size();
-            
+
             TreeNode curr = nodes.pop();
-                int curr_max = map.get(curr);
-                
-                if(curr.val >= curr_max)
-                {
-                    count+=1;
-                    curr_max = curr.val;
-                }
-                
-                if(curr.left!=null)
-                {
-                    nodes.push(curr.left);
-                    map.put(curr.left,curr_max);
-                }
-                
-                if(curr.right!=null)
-                {
-                    nodes.push(curr.right);
-                    map.put(curr.right,curr_max);
-                }
-                
-                
+            int curr_max = map.get(curr);
+
+            if (curr.val >= curr_max) {
+                count += 1;
+                curr_max = curr.val;
             }
-        
-        
-        
+
+            if (curr.left != null) {
+                nodes.push(curr.left);
+                map.put(curr.left, curr_max);
+            }
+
+            if (curr.right != null) {
+                nodes.push(curr.right);
+                map.put(curr.right, curr_max);
+            }
+
+
+        }
+
+
+
         return count;
     }
 }
