76b069ce0162fa7008a28121a887c23e110fd4d6
OmarFarag95 <53435988+OmarFarag95@users.noreply.github.com>
2022-09-02 13:36:58 +0200

    Update 62-unique-paths.java

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,27 +1,22 @@
 class Solution {
-    HashMap<String,Integer> memo = new HashMap<>();
-    
-    
-    
+    HashMap < String, Integer > memo = new HashMap < > ();
+
     public int uniquePaths(int m, int n) {
-        
-        return helper(m,n,memo);
-        
+        return helper(m, n, memo);
     }
-    
-    public int helper(int m, int n, HashMap<String, Integer> memo)
-    {
-        if(m==1 || n==1)
+
+    public int helper(int m, int n, HashMap < String, Integer > memo) {
+        if (m == 1 || n == 1)
             return 1;
-        
-        if(m==0 || n==0)
+
+        if (m == 0 || n == 0)
             return 0;
-        
-        if(memo.containsKey(m+","+n))
-            return memo.get(m+","+n);
-        
-        memo.put(m+","+n, helper(m-1,n,memo) + helper(m,n-1,memo));
-        
-        return memo.get(m+","+n);
+
+        if (memo.containsKey(m + "," + n))
+            return memo.get(m + "," + n);
+
+        memo.put(m + "," + n, helper(m - 1, n, memo) + helper(m, n - 1, memo));
+
+        return memo.get(m + "," + n);
     }
 }
