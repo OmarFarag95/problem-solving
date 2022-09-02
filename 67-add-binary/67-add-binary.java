60e1b1f1e28db6c03e7e97ba9d781ac481d4b835
OmarFarag95 <53435988+OmarFarag95@users.noreply.github.com>
2022-09-02 02:18:34 +0200

    Time: 5 ms (36.62%), Space: 42.7 MB (68.31%) - LeetHub

---
+++ 67-add-binary/67-add-binary.java
@@ -0,0 +1,56 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        
+        StringBuilder res =  new StringBuilder();
+        int i = a.length()-1;
+        int j = b.length()-1;
+        
+        
+        int x = 0;
+        int y = 0;
+        
+        int rem = 0;
+        while(i>=0 || j>=0)
+        {
+            if(i>=0)
+                x = Character.getNumericValue(a.charAt(i));
+            else x = 0;
+            
+            if(j>=0)
+                y = Character.getNumericValue(b.charAt(j));
+            else y = 0;
+            
+            int sum = x+y+rem;
+            
+            if(sum==2)
+            {
+                res.append('0');
+                rem = 1;
+            }
+            else if(sum==3)
+            {
+                res.append('1');
+                rem = 1;
+            }
+            else if (sum==1)
+            {
+                res.append('1');
+                rem = 0;
+            }
+            else 
+            {
+                res.append('0');
+                rem = 0;
+            }
+            
+            i--;
+            j--;
+        }
+        
+        System.out.println(rem);
+        if(rem==1)
+            res.append(1);
+        
+        return res.reverse().toString();
+    }
+}
