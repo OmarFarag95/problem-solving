567c775ce1bcf2ff511cf40d0ef8730910a19861
OmarFarag95 <53435988+OmarFarag95@users.noreply.github.com>
2022-09-17 00:44:22 +0200

    Time: 2499 ms (5.07%), Space: 99.2 MB (80.04%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.java
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.java
@@ -1,20 +1,22 @@
 class Solution {
-    int N, M;
+    
     public int maximumScore(int[] nums, int[] multipliers) {
-        N = nums.length;
-        M = multipliers.length;
-	    return helper(nums, multipliers, 0, 0, new Integer[M][M]);
+        int n = nums.length;
+        int m = multipliers.length;
+	    int max = helper(nums, multipliers, m,n, 0, 0, new int[m][m]);
+        
+        return max;
     }
 
-    private int helper(int[] nums, int[] multipliers, int left, int index, Integer[][] dp) {
-	    int right = N - 1 - (index - left);
-	    if (index == M) return 0;
+    private int helper(int[] nums, int[] multipliers, int m, int n, int left, int index, int[][] dp) {
+	    int right = n - 1 - (index - left);
+	    if (index == m) return 0;
 
-	    if (dp[left][index] != null) return dp[left][index];
+	    if (dp[left][index] != 0) return dp[left][index];
 
 	    int res = Math.max(
-            nums[left] * multipliers[index] + helper(nums, multipliers, left+1, index+1, dp), 
-            nums[right] * multipliers[index] + helper(nums, multipliers, left, index+1, dp));
+            nums[left] * multipliers[index] + helper(nums, multipliers,m,n, left+1, index+1, dp), 
+            nums[right] * multipliers[index] + helper(nums, multipliers,m,n, left, index+1, dp));
 
         dp[left][index] = res;
 	    return res;
