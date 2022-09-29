f249920a6ef17cf0485a80f32f89d6a75b38325c
OmarFarag95 <53435988+OmarFarag95@users.noreply.github.com>
2022-09-29 08:39:31 +0200

    Time: 20 ms (35.73%), Space: 62.5 MB (49.57%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.java
+++ 658-find-k-closest-elements/658-find-k-closest-elements.java
@@ -1,93 +1,72 @@
 class Solution {
-    public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        
-        List<Integer> output = new ArrayList<>();
-        
-        if(x<arr[0])
-        {
-            for(int i=0;i<arr.length;i++)
-            {
-                if(k!=0)
+    public List < Integer > findClosestElements(int[] arr, int k, int x) {
+
+        List < Integer > output = new ArrayList < > ();
+
+        if (x < arr[0]) {
+            for (int i = 0; i < arr.length; i++) {
+                if (k != 0)
                     output.add(arr[i]);
                 else break;
                 k--;
             }
-        }
-        
-        else if(x>arr[arr.length-1])
-        {
-            for(int i=arr.length-k;i<arr.length;i++)
-            {
-                if(k!=0)
+        } else if (x > arr[arr.length - 1]) {
+            for (int i = arr.length - k; i < arr.length; i++) {
+                if (k != 0)
                     output.add(arr[i]);
                 else break;
                 k--;
             }
-        }
-        
-        else 
-        {
+        } else {
             int index = Arrays.binarySearch(arr, x);
-            
-            if (index <0)
-            {
-                for(int i=0;i<arr.length-1;i++)
-                {
-                    if(x>=arr[i] && x<=arr[i+1])
-                    {
+
+            if (index < 0) {
+                for (int i = 0; i < arr.length - 1; i++) {
+                    if (x >= arr[i] && x <= arr[i + 1]) {
                         index = i;
                         break;
                     }
                 }
             }
-            
+
             int l = index;
-            int r = index+1;
-            
-            
-            while(k!=0)
-            {
+            int r = index + 1;
+
+
+            while (k != 0) {
                 int ldiff = Integer.MAX_VALUE;
                 int rdiff = Integer.MAX_VALUE;
-                
-                if(l>=0)
-                    ldiff = Math.abs(arr[l]-x);
-                
-                if (r<=arr.length-1)
-                    rdiff = Math.abs(arr[r]-x);
-                
-                if(ldiff<rdiff)
-                {
+
+                if (l >= 0)
+                    ldiff = Math.abs(arr[l] - x);
+
+                if (r <= arr.length - 1)
+                    rdiff = Math.abs(arr[r] - x);
+
+                if (ldiff < rdiff) {
                     output.add(arr[l]);
                     l--;
-                }
-                else if(ldiff>rdiff)
-                {
+                } else if (ldiff > rdiff) {
                     output.add(arr[r]);
                     r++;
-                }
-                else
-                {
-                    if(arr[l]<x)
-                    {
+                } else {
+                    if (arr[l] < x) {
                         output.add(arr[l]);
                         l--;
-                    }
-                    else
-                    {
+                    } else {
                         output.add(arr[r]);
                         r++;
                     }
                 }
-                
+
                 k--;
-                
+
             }
         }
-        
+
         Collections.sort(output);
-        
+
         return output;
-        
+
     }
 }
