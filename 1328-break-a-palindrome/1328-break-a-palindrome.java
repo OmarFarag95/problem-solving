91e54cf503140721aae540af02aa3fef06e24d02
OmarFarag95 <53435988+OmarFarag95@users.noreply.github.com>
2022-10-10 23:41:38 +0200

    Time: 0 ms (100.00%), Space: 40 MB (98.29%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.java
+++ 1328-break-a-palindrome/1328-break-a-palindrome.java
@@ -1,18 +1,23 @@
 class Solution {
     public String breakPalindrome(String palindrome) {
         
-        if(palindrome.length()==1)
+        char [] letters = palindrome.toCharArray();
+        
+        if(letters.length==1)
             return "";
         
-        for(int i=0;i<palindrome.length()/2;i++)
+        for(int i=0;i<letters.length/2;i++)
         {
-            if(palindrome.charAt(i)!='a')
-                return palindrome.substring(0,i) + 'a' + palindrome.substring(i+1);
+            if(letters[i]!='a')
+            {
+                letters[i] = 'a';
+                return String.valueOf(letters);
+            }
+                
         }
         
-        
-        
-        return palindrome.substring(0,palindrome.length()-1) + 'b';
+        letters[letters.length-1] = 'b';
+        return String.valueOf(letters);
         
     }
 }
