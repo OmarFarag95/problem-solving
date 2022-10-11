954e86b1b7115c584a086598ff6a48dbc7fff27e
OmarFarag95 <53435988+OmarFarag95@users.noreply.github.com>
2022-10-11 23:17:18 +0200

    Time: 168 ms (81.90%), Space: 14.2 MB (5.33%) - LeetHub

---
+++ 1995-count-special-quadruplets/1995-count-special-quadruplets.py
@@ -0,0 +1,29 @@
+class Solution:
+    def countQuadruplets(self, nums: List[int]) -> int:
+        
+        map = {}
+        values = set()
+        
+        count = 0
+        
+        for a in range(0,len(nums)):
+            for b in range(a+1, len(nums)):
+                curr_sum = nums[a]+nums[b]
+                map[curr_sum] = map.get(curr_sum,[]) + [b]
+        
+        print(map)
+        for c in range(0,len(nums)):
+            for d in range(c+1, len(nums)):
+                curr_sum = (nums[d] - nums[c])
+                
+                if curr_sum in map:
+                    values = map.get(curr_sum)
+                    #loop over tuples
+                    for p in values:
+                        if c>p:
+                            count+=1
+        
+        return count
+        
+        
+        
