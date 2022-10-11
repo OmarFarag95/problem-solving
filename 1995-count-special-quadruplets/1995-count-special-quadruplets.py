71c46ef69e8d111b0cd99dc8dcf941d7cd6be342
OmarFarag95 <53435988+OmarFarag95@users.noreply.github.com>
2022-10-11 23:18:07 +0200

    Time: 199 ms (78.83%), Space: 13.9 MB (65.27%) - LeetHub

--- 1995-count-special-quadruplets/1995-count-special-quadruplets.py
+++ 1995-count-special-quadruplets/1995-count-special-quadruplets.py
@@ -13,11 +13,11 @@ class Solution:
         
         for c in range(0,len(nums)):
             for d in range(c+1, len(nums)):
-                curr_sum = (nums[d] - nums[c])
+                curr_diff = nums[d] - nums[c]
                 
-                if curr_sum in map:
-                    values = map.get(curr_sum)
-                    #loop over tuples
+                if curr_diff in map:
+                    values = map.get(curr_diff)
+                    #loop over Bs
                     for p in values:
                         if c>p:
                             count+=1
