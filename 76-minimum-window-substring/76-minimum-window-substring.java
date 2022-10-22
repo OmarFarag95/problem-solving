49181df3654186456ea048c14a157702bc37dfd3
OmarFarag95 <53435988+OmarFarag95@users.noreply.github.com>
2022-10-22 04:01:35 +0200

    Time: 35 ms (30.15%), Space: 46.8 MB (45.24%) - LeetHub

---
+++ 76-minimum-window-substring/76-minimum-window-substring.java
@@ -0,0 +1,60 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        
+        if(s.length()<t.length())
+            return "";
+        
+        HashMap<Character,Integer> counts = new HashMap<>();
+        
+        for(int i=0;i<t.length();i++)
+            counts.put(t.charAt(i), counts.getOrDefault(t.charAt(i),0)+1);
+        
+        //minimum is [i, i+t.length()]
+        
+        int l = 0;
+        int r = 0;
+        
+        HashMap<Character,Integer> curr_counts = new HashMap<>();
+       
+        int length = 0;
+        
+        int min_size = Integer.MAX_VALUE;
+        int final_l = -1;
+        int final_r = -1;
+        
+        while(r<s.length() && l<=r)
+        {
+            curr_counts.put(s.charAt(r), curr_counts.getOrDefault(s.charAt(r),0)+1);
+            
+            if(curr_counts.get(s.charAt(r)).equals(counts.getOrDefault(s.charAt(r),0)))
+                length+=1;
+                
+            // shrink window if match is found
+            while(length == counts.size())
+            {
+                if(r-l+1<min_size)
+                {
+                    min_size = r-l+1;
+                    final_l = l;
+                    final_r = r;
+                }
+                
+                //remove character at current_position l
+                curr_counts.put(s.charAt(l), curr_counts.get(s.charAt(l))-1);
+                
+                if(curr_counts.get(s.charAt(l)) < counts.getOrDefault(s.charAt(l),0))
+                    length-=1;
+                
+                l++;
+            }
+            
+            r++;
+        }
+        
+        if(final_l!=-1)
+            return s.substring(final_l, final_r+1);
+        
+        return "";
+        
+    }
+}
