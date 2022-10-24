6c6194d04d14175d152a9bbb6c032d3e8ea2cdef
OmarFarag95 <53435988+OmarFarag95@users.noreply.github.com>
2022-10-25 00:11:36 +0200

    Time: 15 ms (81.66%), Space: 42.2 MB (83.22%) - LeetHub

---
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.java
@@ -0,0 +1,43 @@
+class Solution {
+    
+    int max = 0;
+    public int maxLength(List<String> arr) {
+        helper(arr,"",0);
+        
+        return max;
+        
+    }
+    
+    public void helper(List<String> arr, String curr, int start)
+    {
+        if(curr.length()>max)
+            max = curr.length();
+        
+        for(int i = start; i<arr.size(); i++)
+        {
+            String candidate = arr.get(i);
+            HashSet<Character> unique = new HashSet<>();
+            boolean invalid = false;
+            for(int j=0;j<candidate.length();j++)
+            {
+                if(unique.contains(candidate.charAt(j)))
+                {
+                    invalid = true;
+                    break;
+                }
+                else unique.add(candidate.charAt(j));
+                
+                if(curr.contains(candidate.charAt(j)+""))
+                {
+                    invalid = true;
+                    break;
+                }
+            }
+            
+            if(invalid)
+                continue;
+            
+            helper(arr, curr+arr.get(i), i+1);
+        }
+    }
+}
