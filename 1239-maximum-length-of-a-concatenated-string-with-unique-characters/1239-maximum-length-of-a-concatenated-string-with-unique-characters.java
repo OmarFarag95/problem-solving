0d235360f8d08a91f4badabd99982998d91a399a
OmarFarag95 <53435988+OmarFarag95@users.noreply.github.com>
2022-10-25 00:12:17 +0200

    Time: 31 ms (63.86%), Space: 53.7 MB (50.35%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.java
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.java
@@ -1,43 +1,37 @@
 class Solution {
-    
+
     int max = 0;
-    public int maxLength(List<String> arr) {
-        helper(arr,"",0);
-        
+    public int maxLength(List < String > arr) {
+        helper(arr, "", 0);
+
         return max;
-        
+
     }
-    
-    public void helper(List<String> arr, String curr, int start)
-    {
-        if(curr.length()>max)
+
+    public void helper(List < String > arr, String curr, int start) {
+        if (curr.length() > max)
             max = curr.length();
-        
-        for(int i = start; i<arr.size(); i++)
-        {
+
+        for (int i = start; i < arr.size(); i++) {
             String candidate = arr.get(i);
-            HashSet<Character> unique = new HashSet<>();
+            HashSet < Character > unique = new HashSet < > ();
             boolean invalid = false;
-            for(int j=0;j<candidate.length();j++)
-            {
-                if(unique.contains(candidate.charAt(j)))
-                {
+            for (int j = 0; j < candidate.length(); j++) {
+                if (unique.contains(candidate.charAt(j))) {
                     invalid = true;
                     break;
-                }
-                else unique.add(candidate.charAt(j));
-                
-                if(curr.contains(candidate.charAt(j)+""))
-                {
+                } else unique.add(candidate.charAt(j));
+
+                if (curr.contains(candidate.charAt(j) + "")) {
                     invalid = true;
                     break;
                 }
             }
-            
-            if(invalid)
+
+            if (invalid)
                 continue;
-            
-            helper(arr, curr+arr.get(i), i+1);
+
+            helper(arr, curr + arr.get(i), i + 1);
         }
     }
 }
