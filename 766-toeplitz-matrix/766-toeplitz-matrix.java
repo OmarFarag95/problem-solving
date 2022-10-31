6eb3a383754684fe191a5c141e638f43fb63e5c7
OmarFarag95 <53435988+OmarFarag95@users.noreply.github.com>
2022-10-31 21:26:31 +0100

    Time: 1 ms (93.76%), Space: 45.5 MB (74.15%) - LeetHub

--- 766-toeplitz-matrix/766-toeplitz-matrix.java
+++ 766-toeplitz-matrix/766-toeplitz-matrix.java
@@ -1,23 +1,35 @@
 class Solution {
-    public boolean isToeplitzMatrix(int[][] matrix) {
+//     public boolean isToeplitzMatrix(int[][] matrix) {
         
-        HashMap<Integer, Integer> map = new HashMap<>();
+//         HashMap<Integer, Integer> map = new HashMap<>();
+        
+//         for(int i=0;i<matrix.length;i++)
+//         {
+//             for(int j = 0; j<matrix[i].length;j++)
+//             {
+                
+//                     if(map.containsKey(j-i))
+//                     {
+//                         if(map.get(j-i) != matrix[i][j])
+//                             return false;
+//                     }
+//                     else
+//                     {
+//                         map.put(j-i, matrix[i][j]);
+//                     }
+                
+//             }
+//         }
+//         return true;
+//     }
+    public boolean isToeplitzMatrix(int[][] matrix) {
         
         for(int i=0;i<matrix.length;i++)
         {
             for(int j = 0; j<matrix[i].length;j++)
             {
-                
-                    if(map.containsKey(j-i))
-                    {
-                        if(map.get(j-i) != matrix[i][j])
-                            return false;
-                    }
-                    else
-                    {
-                        map.put(j-i, matrix[i][j]);
-                    }
-                
+                if(i > 0 && j > 0 && matrix[i][j]!=matrix[i-1][j-1])
+                    return false;
             }
         }
         return true;
