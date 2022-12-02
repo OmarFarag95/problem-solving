c6f6390d94deb20b65e72f7aaeb41f5580f70ebe
OmarFarag95 <53435988+OmarFarag95@users.noreply.github.com>
2022-12-02 01:25:00 +0100

    Time: 11 ms (38.73%), Space: 42.7 MB (21.81%) - LeetHub

---
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean halvesAreAlike(String s) {
+        
+        if(s.length()%2!=0)
+            return false;
+        
+        //s = s.toLowerCase();
+        int l =0;
+        int r =0;
+        
+        HashSet<Character> vowels = new HashSet<>(){{
+            add('a');
+            add('e');
+            add('i');
+            add('o');
+            add('u');
+            add('A');
+            add('E');
+            add('I');
+            add('O');
+            add('U');
+            }};
+        
+        int i =0;
+        int j = s.length()/2;
+        
+        while(i<s.length()/2)
+        {
+            if(vowels.contains(s.charAt(i)))
+                l++;
+            if(vowels.contains(s.charAt(j)))
+                r++;
+            i++;
+            j++;
+        }
+        
+        
+        return l==r;
+    }
+}
