b711baa1ef7d00336ee2e79f23b17e9d92b25f1d
OmarFarag95 <53435988+OmarFarag95@users.noreply.github.com>
2023-01-04 00:11:01 +0200

    Time: 248 ms (5.18%), Space: 45.4 MB (64.41%) - LeetHub

---
+++ 944-delete-columns-to-make-sorted/944-delete-columns-to-make-sorted.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int minDeletionSize(String[] strs) {
+        
+        HashMap<Integer,Character> map = new HashMap<>();
+        
+        HashSet<Integer> columns = new HashSet<>();
+        for(int i=0;i<strs.length;i++)
+        {
+            String word = strs[i];
+            
+            int k = 0;
+            for(int j=0;j<word.length();j++)
+            {
+            
+            if(!map.containsKey(k))
+                map.put(k, word.charAt(k));
+            else
+            {
+                if(word.charAt(k)< map.get(k) && !columns.contains(k))
+                    columns.add(k);
+                else
+                    map.put(k, word.charAt(k));
+            }
+                k++;
+            }
+            
+            System.out.println(map);
+        }
+        
+        return columns.size();
+    }
+}
