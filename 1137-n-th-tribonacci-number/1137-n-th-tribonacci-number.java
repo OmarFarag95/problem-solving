f7f54ff45d3bf76ecd4c37ef2e55e75c21b032a9
OmarFarag95 <53435988+OmarFarag95@users.noreply.github.com>
2023-01-31 01:30:27 +0200

    Time: 0 ms (100.00%), Space: 39.4 MB (41.58%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -1,22 +1,48 @@
 class Solution {
+//     public int tribonacci(int n) {
+        
+//         if(n==0)
+//             return 0;
+//         if(n==1 || n==2)
+//             return 1;
+        
+//         int [] fib = new int[n+1];
+//         fib[0] = 0;
+//         fib[1] = 1;
+//         fib[2] = 1;
+        
+//         for(int i=3;i<=n;i++)
+//         {
+//             fib[i] = fib[i-1] + fib[i-2] + fib[i-3]; 
+//         }
+        
+//         return fib[n];
+        
+//     }
     public int tribonacci(int n) {
         
+        int a = 0;
+        int b = 1;
+        int c = 1;
+        int d = 1;
         if(n==0)
             return 0;
         if(n==1 || n==2)
             return 1;
         
-        int [] fib = new int[n+1];
-        fib[0] = 0;
-        fib[1] = 1;
-        fib[2] = 1;
-        
-        for(int i=3;i<=n;i++)
+       
+        int i = 2;
+        while(i<n)
         {
-            fib[i] = fib[i-1] + fib[i-2] + fib[i-3]; 
+            d = a+b+c;
+            a = b;
+            b = c;
+            c = d;
+            i++;
+            
         }
         
-        return fib[n];
+        return c;
         
     }
 }
