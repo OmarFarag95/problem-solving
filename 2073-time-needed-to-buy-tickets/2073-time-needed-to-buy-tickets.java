4f556c314ef8e57b00025346b6daa0f2769396fe
OmarFarag95 <53435988+OmarFarag95@users.noreply.github.com>
2024-04-13 17:57:29 +0200

    Time: 2 ms (45.53%), Space: 41.3 MB (37.70%) - LeetHub

---
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int timeRequiredToBuy(int[] tickets, int k) {
+        
+        
+        int t = 0;
+        while(tickets[k]!=0)
+        {
+            for(int i=0; i<tickets.length; i++)
+            {
+                if(tickets[i] > 0)
+                {
+                    tickets[i]-=1;
+                    t+=1;
+                }
+                if(tickets[k]==0)
+                    break;
+            }
+        }
+        
+        return t;
+    }
+}
